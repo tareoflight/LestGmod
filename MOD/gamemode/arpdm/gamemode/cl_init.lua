a43439f579cc00698e154b034a4adc8f775a3eab
tareoflight <nathan.rochon@gmail.com>
2017-02-22 18:56:39 -0500

    Update cl_init.lua

--- MOD/gamemode/arpdm/gamemode/cl_init.lua
+++ MOD/gamemode/arpdm/gamemode/cl_init.lua
@@ -3,15 +3,17 @@ include( "shared.lua" )
 -- ----------------------
 
 
-local frame = vgui.Create( "DFrame" )
-frame:SetSize( 300, 250 )
-frame:Center()
-frame:MakePopup()
+local DermaPanel = vgui.Create( "DFrame" )
+DermaPanel:SetPos( 100, 100 )
+DermaPanel:SetSize( 300, 200 )
+DermaPanel:SetTitle( "buttions in Frames" )
+DermaPanel:SetDraggable( true )
+DermaPanel:MakePopup()
 
-local DermaButton = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-DermaButton:SetText( "Say hi" )					// Set the text on the button
-DermaButton:SetPos( 25, 50 )					// Set the position on the frame
-DermaButton:SetSize( 250, 30 )					// Set the size
-DermaButton.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
-	RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
+local DermaButton = vgui.Create( "DButton", DermaPanel ) 
+DermaButton:SetText( "Say hi" )					
+DermaButton:SetPos( 25, 50)
+DermaButton:SetSize( 250, 30 )	
+DermaButton.DoClick = function()
+	RunConsoleCommand( "say", "Hi" )
 end
