a255850c7c24a76e5adcee48eeb7990c6e3151d7
TheOriginalPoit <dahrouge08@gmail.com>
2017-02-22 20:43:12 -0700

    Player Greeting
    Attempting to greet a player upon server connection.

--- MOD/gamemode/pRPDM/gamemode/init.lua
+++ MOD/gamemode/pRPDM/gamemode/init.lua
@@ -2,3 +2,17 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+// Attempting to greet the player upon server connection.
+
+poitsip = 205.206.8.249
+
+function GM:PlayerConnect( name, ip )
+	if ip = 205.206.8.249
+		then
+			PrintMessage( HUD_PRINTTALK, "Welcome back to the server, Poit!" )
+		else
+			PrintMessage( HUD_PRINTTALK, name .. " has joined the game." )
+
+end
+
