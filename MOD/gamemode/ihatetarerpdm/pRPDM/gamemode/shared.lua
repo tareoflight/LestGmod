f461c5d33519be6bc038783abe7f14c30a033bfe
TheOriginalPoit <dahrouge08@gmail.com>
2017-02-22 20:06:40 -0700

    My Folders
    I put my folders on github so tare can help me

---
+++ MOD/gamemode/ihatetarerpdm/pRPDM/gamemode/shared.lua
@@ -0,0 +1,10 @@
+DeriveGamemode( "sandbox" )
+
+GM.Name = "Ajax RPDM"
+GM.Author = "Poit / Tareoflight"
+GM.Email = "dahrouge08@gmail.com"
+GM.Website = "ajaxstudios.com"
+
+function GM:Initialize()
+
+end
